e3ec5e722b46f64c54f72e194fcabe9946722497
seokjun <lsjj9219@gmail.com>
2025-01-30 11:31:45 +0900

    무한로딩2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 export default function Home() {
   return (
     <div>
-      <h2>Hello World</h2>
+      <h2>Hello World2</h2>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
