72053ad2051b41554141fd899b6547551cc2916b
seokjun <lsjj9219@gmail.com>
2025-01-30 11:30:40 +0900

    무한로딩딩

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello World</h2>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
